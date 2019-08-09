68e99b9ac97658541ed88e9c044cc897a56be629
Gabriiell <gabriel.opereyra@gmail.com>
2019-08-09 16:25:09 -0300

    Start mapping service implementation

--- src/Library.API/Services/PropertyMappingService.cs
+++ src/Library.API/Services/PropertyMappingService.cs
@@ -1,11 +1,39 @@
-﻿using System;
+﻿using Library.API.Entities;
+using Library.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Library.API.Services
 {
-    public class PropertyMappingService
+    public class PropertyMappingService : IPropertyMappingService
     {
+        private Dictionary<string, PropertyMappingValue> _authorPropertyMapping = new Dictionary<string, PropertyMappingValue>(StringComparer.OrdinalIgnoreCase)
+        {
+            { "Id", new PropertyMappingValue(new List<string>() { "Id" }) },
+            { "Genre", new PropertyMappingValue(new List<string>() { "Genre" }) },
+            { "Age", new PropertyMappingValue(new List<string>() { "DateOfBirth" }) },
+            { "Name", new PropertyMappingValue(new List<string>() { "FirstName", "LastName" }) }
+        };
+
+        private IList<IPropertyMapping> _propertyMappings = new List<IPropertyMapping>();
+
+        public PropertyMappingService()
+        {
+            _propertyMappings.Add(new PropertyMapping<AuthorDto, Author>(_authorPropertyMapping));
+        }
+
+        public Dictionary<string, PropertyMappingValue> GetPropertyMapping<TSource, TDestination>()
+        {
+            var matchingMapping = _propertyMappings.OfType<PropertyMapping<TSource, TDestination>>();
+
+            if (matchingMapping.Count() != 1)
+            {
+                throw new Exception($"Cannot find exact property mapping instance for <{typeof(TSource)}, {typeof(TDestination)}>");
+            }
+
+            return matchingMapping.First()._mappingDictionary;
+        }
     }
 }
